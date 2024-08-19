5bc65acaaf224ce5c64bb0dfedabc9b6a0d0a20a
Ikenna Nnorom <ikenna.nnorom12@gmail.com>
2024-08-19 10:56:31 +0100

    first commit of files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./routes/layout/layout";
+import HomePage from "./routes/homePage/homePage";
+import SinglePage from "./routes/singlePage/singlePage";
+import ListPage from "./routes/listPage/listPage";
+import Login from "./routes/login/login";
+import ProfilePage from "./routes/profilePage/profilePage";
+
 function App() {
-  return (
-    <div>Hello World</div>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+        {
+          path: "/profile",
+          element: <ProfilePage />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/routes/login/login.jsx
@@ -0,0 +1,6 @@
+import "./login.scss";
+
+const Login = () => {
+  return <div>login</div>;
+};
+export default Login;
