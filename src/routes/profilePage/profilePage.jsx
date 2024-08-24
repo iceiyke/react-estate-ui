fe0d1bfde3d857bcb6312935a3084cf0f01fac59
Ikenna Nnorom <ikenna.nnorom12@gmail.com>
2024-08-24 08:46:27 +0100

    2nd commit, end of 1st video

--- src/routes/profilePage/profilePage.jsx
+++ src/routes/profilePage/profilePage.jsx
@@ -1,4 +1,46 @@
+import Chat from "../../components/chat/Chat";
+import List from "../../components/list/List";
+import "./profilePage.scss";
+
 const ProfilePage = () => {
-  return <div>Profile Page</div>;
+  return (
+    <div className="profilePage">
+      <div className="details">
+        <div className="wrapper">
+          <div className="title">
+            <h2>User Information</h2>
+            <button>Update Profile</button>
+          </div>
+          <div className="info">
+            <span>
+              Avatar:
+              <img
+                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+              />
+            </span>
+            <span>
+              Username: <b>John Doe</b>
+            </span>
+            <span>Email: john@gmail.com</span>
+          </div>
+          <div className="title">
+            <h2>My List</h2>
+            <button>Create New Post</button>
+          </div>
+          <List />
+          <div className="title">
+            <h2>Saved List</h2>
+          </div>
+          <List />
+        </div>
+      </div>
+      <div className="chatContainer">
+        <div className="wrapper">
+          <Chat />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default ProfilePage;
